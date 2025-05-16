e55077fe19614b5cac075265d6bd072f9c087341
Ostrogliad <ostroglv@students.dsbn.org>
2025-05-16 12:50:54 -0400

    map tile change and sizes adjust

--- Assets/Main Game/player stuff/mouseinput.cs
+++ Assets/Main Game/player stuff/mouseinput.cs
@@ -19,21 +19,13 @@ public class mouseinput : MonoBehaviour
         private int RayHitPoint;
         private Vector3 ScreenResol;
         private Vector3 MousePos;
-        //private float screenH;
-        //private float screenW;
+
         
-
-
-        //  GridLayout gridLayout = transform.parent.GetComponent<GridLayout>();
         // Start is called before the first frame update
         void Start()
         {
-        //screenH = (Screen.height / 2);
-        //screenW = (Screen.width / 2);
-        //ScreenResol = new Vector3(screenW, screenH, 0f);
-            //Debug.Log("s");
+        
 
-            //Debug.DrawLine(Vector3.zero, new Vector3(5, 0, 0), Color.white, 2.5f);
             Debug.Log(grid); 
             Debug.Log(camTransf);
             Debug.Log(camCam);
@@ -45,26 +37,16 @@ public class mouseinput : MonoBehaviour
             if (Input.GetMouseButtonDown(0))
             {
             
-            //camCam = Camera.main;
                 MousePos = Input.mousePosition;
-                Debug.Log(MousePos + "MOUSE POS");
-                //Debug.Log("CellInPosition" + cam.transform.position);
-                //Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-                //float angle = Vector3.Angle(camera.transform.forward, ray.direction);
-                CellInPosition = camCam.ScreenToWorldPoint(MousePos) ; //(Input.mousePosition - ScreenResol) + cam.transform.position; //Physics2D.Raycast(ray.origin, ray.direction);
-                CellInPosition.z = CellInPosition.z * 0.0f;
-                //Debug.DrawRay(cam.transform.positi;on, ray.origin, Color.white, 1f);
-                Debug.Log(CellInPosition + "CEll");
+                //Debug.Log(MousePos + "MOUSE POS");
+
+                CellInPosition = camCam.ScreenToWorldPoint(MousePos);
+                //Debug.Log(CellInPosition + "CEll");
+
                 PositionInCell = grid.WorldToCell(CellInPosition);
-                Debug.Log(PositionInCell + "Positoon");
+                //Debug.Log(PositionInCell + "Positoon");
+
                 Instantiate(cubePrefab, PositionInCell * 5, Quaternion.identity);
-                
-
-                //Debug.Log("CellInPosition" + ray);
-                //Debug.Log("CellInPosition" + CellInPosition);
-
-                //Debug.Log("PositionInCell" + PositionInCell);
-                
 
             }
     } 
