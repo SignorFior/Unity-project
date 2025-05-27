ce8239e8cb6035b2345a43b044edf68d1f050c48
Ostrogliad <ostroglv@students.dsbn.org>
2025-05-27 12:51:55 -0400

    L

--- Assets/Main Game/player stuff/mouseinput.cs
+++ Assets/Main Game/player stuff/mouseinput.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using Unity.VisualScripting;
 using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -11,21 +12,23 @@ using UnityEngine.WSA;
 public class mouseinput : MonoBehaviour
 {
         public Grid grid;
-        private Vector3 PositionInCell;
-        private Vector3 CellInPosition;
+        private Vector3 WorldInCell;
+        private Vector3 MouseInWorld;
         public Camera camCam;
         public GameObject cubePrefab;
         private int RayHitPoint;
         private Vector3 ScreenResol;
         private Vector3 MousePos;
+        public Transform PlayerPosition;
+        private Vector3 centering = new Vector2(0.5f, 0.5f);
 
         
         // Start is called before the first frame update
         void Start()
         {
         
-            Debug.Log(grid); 
-            Debug.Log(camCam);
+            //Debug.Log(grid); 
+            //Debug.Log(camCam);
         }
 
         // Update is called once per frame
@@ -33,19 +36,31 @@ public class mouseinput : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(0))
             {
-            
+                
                 MousePos = Input.mousePosition;
                 //Debug.Log(MousePos + "MOUSE POS");
 
-                CellInPosition = camCam.ScreenToWorldPoint(MousePos);
-                //Debug.Log(CellInPosition + "CEll");
+                MouseInWorld = camCam.ScreenToWorldPoint(MousePos);
+                //Debug.Log(MouseInWorld + "CEll");
 
-                PositionInCell = grid.WorldToCell(CellInPosition);
-                //Debug.Log(PositionInCell + "Positoon");
+                WorldInCell = grid.WorldToCell(MouseInWorld);
+                //Debug.Log(WorldInCell + "Positoon");
+                RaycastHit2D hit = Physics2D.Raycast(WorldInCell + centering, PlayerPosition.transform.position, 0.9f);
+                Debug.DrawRay(WorldInCell, PlayerPosition.transform.position + centering, Color.blue, 1f);
+                //Debug.Log(WorldInCell + "World Cell    " );
+                Debug.Log(PlayerPosition.transform.position + centering);
+                Debug.Log("Player Positioning menus centring    " + PlayerPosition.transform.position);
+                if (hit)
+                {
+                    Debug.Log(hit.collider);
+                }
+                else
+                {
+                    Instantiate(cubePrefab, WorldInCell, Quaternion.identity);
+                }
+                
 
-                Instantiate(cubePrefab, PositionInCell, Quaternion.identity);
-
-            }
+                }
         }
         void FixedUpdate()
         {
